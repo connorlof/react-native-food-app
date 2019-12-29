e4c1815248e2420a7738fb81ff1154f51da8001b
Connor Loftus <connorlof@gmail.com>
2019-12-28 19:31:18 -0500

    Displaying pictures on detail screen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,15 +1,45 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+    const [result, setResult] = useState(null);
+    const id = navigation.getParam('id');
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect(() =>{
+        getResult(id);
+    }, []);
+
+    if(!result) {
+        return null;
+    }
 
     return (
         <View>
-            <Text>DetailsScreen</Text>
+            <Text>{result.name}</Text>
+            <FlatList 
+                data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={({ item }) => {
+                    return <Image style={styles.imageStyle} source={{ uri: item}} />
+                }}
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    imageStyle: {
+        width:250,
+        height: 120,
+        borderRadius: 4,
+        margin: 10
+    }
+});
 
 export default ResultsShowScreen;
